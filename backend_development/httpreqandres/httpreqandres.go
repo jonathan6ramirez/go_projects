82cd02e46ff44659a492f538c3d127ab3a7b45ea
Jonathan Ramirez Torres <jramirez@ntcc.edu>
2025-02-20 17:45:48 -0600

    Adding functions to the file that parse requests and responses

--- backend_development/httpreqandres/httpreqandres.go
+++ backend_development/httpreqandres/httpreqandres.go
@@ -193,3 +193,240 @@ func (r *Request) WriteTo(w io.Writer) (n int64, err error) {
   err = printf("%s\r\n", r.Body)
   return n, err
 }
+
+func (resp *Response) WriteTo(w io.Writer) (n int64, err error) {
+  printf := func(format string, args, ...any) error {
+    m, err := fmt.Fprintf(w, format, args...)
+    n += int64(m)
+    return err
+  }
+  if err := printf("HTTP/1.1 %d %s\r\n", resp.StatusCode, http.StatusText(resp.StatusCode)); err != nil {
+    return n, err
+  }
+
+  for _, h := range resp.Headers {
+    if err := printf("%s: %s\r\n", h.Key, h.Value); err != nil {
+      return n, err
+    }
+  }
+
+  if err := printf("\r\n%s\r\n", resp.Body); err != nil {
+    return n, err
+  }
+
+  return n, nil
+}
+
+var _, _ fmt.Stringer = (*Request)(nil), (*Response)(nil) // compile-time check that Request and Response implement fmt.Stringer
+var _, _ encoding.TextMarshaler = (*Request)(nil), (*Response)(nil)
+func (r *Request) String() string { b := new(strings.Builder); r.WriteTo(b); return b.String() }
+func (resp *Response) String() string { b := new(strings.Builder); resp.WriteTo(b); return b.String() }
+func (r *Request) MarshalText() ([]byte, error) { b := new(bytes.Buffer); r.WriteTo(b); return b.Bytes(), nil }
+func (resp *Response) MarshalText() ([]byte, error) { b := new(bytes.Buffer); resp.WriteTo(b); return b.Bytes(), nil }
+
+// ParseRequest parses a HTTP request from the given text.
+func ParseRequest(raw string) (r Request, err error) {
+  // request has three parts:
+  // 1. Request lines
+  // 2. Headers
+  // 3. Body (optional)
+  lines := splitLines(raw)
+
+  log.Println(lines)
+  if len(lines) < 3 {
+    return Request{}, fmt.Errorf("malformed request: should have at least 3 lines")
+  }
+
+  // The first line is special.
+  first := strings.Fields(lines[0])
+  r.Method, r.Path = first[0], first[1]
+  if !strings.HasPrefix(r.Path, "/") {
+    return Request{}, fmt.Errorf("malformed request: path should start with /")
+  }
+  if !strings.Contains(first[2], "HTTP") {
+    return Request{}, fmt.Errorf("malformed request: first line should contain HTTP version")
+  }
+  var foundhost bool
+  var bodyStart int
+  // then we have Headers. up until an empty line.
+  for i := 1; i < len(lines); i++ {
+    if lines[i] == "" { // this means an empty line was found
+      bodyStart = i + 1
+      break
+    }
+    key, val, ok := strings.Cut(lines[i], ": ")
+    if !ok {
+      return Request{}, fmt.Errorf("malfromed request: header %q should be of form 'key: value'", lines[i])
+    }
+    if key == "Host" { // special case: host header is not required.
+      foundhost = true
+    }
+    key = AsTitle(key)
+
+    r.Headers = append(r.Headers, Header{key, val})
+  }
+  end := len(lines) -1 // recombine the body using normal newlines; skip the last empty line.
+  r.Body = strings.Join(lines[bodyStart: end], "\r\n")
+  if !foundhost {
+    return Request{}, fmt.Errorf("malformed request: missing Host header")
+  }
+
+  return r, nil
+}
+
+/// ParseResponse parses the given HTTP/1.1 response string into the Response. It returns an error
+// if the response is invalid,
+// - not a valid integer
+// - invalid status code
+// - missing status text
+// - invalid headers
+// it doesn't properly handle multi-line headers, headers with multiple values, or html-encoding, etc.
+func ParseResponse(raw string) (resp *Response, err error) {
+  // response has three parts:
+  // 1. Response line
+  // 2. Headers
+  // 3. Body (optional)
+  lines := splitLines(raw)
+  log.Println(lines)
+
+  // The first line is special.
+  first := strings.SplitN(lines[0], " ", 3)
+  if !strings.Contains(first[0], "HTTP") {
+    return nil, fmt.Errorf("malformed Response: first line should contain HTTP version")
+  }
+  resp = new(Response)
+  resp.StatusCode, err = strconv.Atoi(first[1])
+  if err != nil {
+    return nil, fmt.Errorf("malformed response: expected status code to be an integer, got %q", first[1])
+  }
+  if first[2] == "" || http.StatusText(resp.StatusCode) != first[2] {
+    log.Printf("missing or incorrect status text for status code %d: expected %q, but got %q", resp.StatusCode, http.StatusText(resp.StatusCode), first[2])
+  }
+  var bodyStart int
+  // then we have headers, up until an empty line.
+  for i := 1; i < len(lines); i++ {
+    log.Println(i, lines[i])
+    if lines[i] == "" {
+      bodyStart = i + 1
+      break
+    }
+    key, val, ok := strings.Cut(lines[i], ": ")
+    if !ok {
+      return nil, fmt.Errorf("malformed response: header %q should be of form 'key: value'", lines[i])
+    }
+    key = AsTitle(key)
+    resp.Headers = append(resp.Headers, Header{key, val})
+  }
+  resp.Body = strings.TrimSpace(strings.Join(lines[bodyStart:], "\r\n")) // recombine the body using normal newlines.
+  return resp, nil
+}
+
+// splitLines on the "\r\n" sequence; multiple separators in a row are NOT collapsed.
+func splitLines(s string) []string {
+  if s == ""  {
+    return nil
+  }
+  var lines []string
+  i := 0
+  for {
+    j := strings.Index(s[i:], "\r\n")
+    if j == -1 {
+      lines = append(lines, s[i:])
+      return lines
+    }
+    lines = append(lines, s[i:i+j]) // up to but not including the \r\n
+    i += j + 2 // skip the \r\n
+  }
+}
+
+func TestHTTPResponse(t *testing.T) {
+  for name, tt := range map[string]struct {
+    input string
+    want *Response
+  }{
+    "200 OK (no body)": {
+      input: "HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n",
+      want: &Response{
+        StatusCode: 200,
+        Headers: []Header{
+          {"Content-Length", "0"},
+        }
+      }
+    },
+    "404 Not Found (w/ body)": {
+      input: "HTTP/1.1 404 Not Found\r\nContent-Length: 11\r\n\r\nHello World\r\n",
+      want: &Response{
+        StatusCode: 404,
+        Headers: []Header{
+          {"Content-Length", "11"},
+        },
+        Body: "Hello World",
+      }
+    },
+  } {
+    t.Run(name, func(t *testing.T) {
+      got, err := ParseResponse(tt.input)
+      if err != nil {
+        t.Errorf("ParseResponse(%q) returned error: %v", tt.input, err)
+      }
+      if !reflect.DeepEqual(got, tt.want) {
+        t.Errorf("ParseResponse(%q) = %#+v, want %#+v", tt.input, got, tt.want)
+      }
+
+      if got2, err := ParseResponse(got.String()); err != nil {
+        t.Errorf("ParseResponse(%q) returned error: %v", got.String(), err)
+      } else if !reflect.DeepEqual(got2, got) {
+        t.Errorf("ParseResponse(%q) = %#+v, want %#+v", got.String(), got2, got)
+      }
+    })
+  }
+}
+
+func TestHTTPRequest(t *testing.T) {
+    for name, tt := range map[string]struct {
+        input string
+        want  Request
+    }{
+        "GET (no body)": {
+            input: "GET / HTTP/1.1\r\nHost: www.example.com\r\n\r\n",
+            want: Request{
+                Method: "GET",
+                Path:   "/",
+                Headers: []Header{
+                    {"Host", "www.example.com"},
+                },
+            },
+        },
+        "POST (w/ body)": {
+            input: "POST / HTTP/1.1\r\nHost: www.example.com\r\nContent-Length: 11\r\n\r\nHello World\r\n",
+            want: Request{
+                Method: "POST",
+                Path:   "/",
+                Headers: []Header{
+                    {"Host", "www.example.com"},
+                    {"Content-Length", "11"},
+                },
+                Body: "Hello World",
+            },
+        },
+    } {
+        t.Run(name, func(t *testing.T) {
+            got, err := ParseRequest(tt.input)
+            if err != nil {
+                t.Errorf("ParseRequest(%q) returned error: %v", tt.input, err)
+            }
+            if !reflect.DeepEqual(got, tt.want) {
+                t.Errorf("ParseRequest(%q) = %#+v, want %#+v", tt.input, got, tt.want)
+            }
+            // test that the request can be written to a string and parsed back into the same request.
+            got2, err := ParseRequest(got.String())
+            if err != nil {
+                t.Errorf("ParseRequest(%q) returned error: %v", got.String(), err)
+            }
+            if !reflect.DeepEqual(got, got2) {
+                t.Errorf("ParseRequest(%q) = %+v, want %+v", got.String(), got2, got)
+            }
+
+        })
+    }
+}
