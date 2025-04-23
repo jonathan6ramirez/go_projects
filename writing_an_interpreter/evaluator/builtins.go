fdbbfa676e3e50998b43229996fdfbd13e526e09
Jonathan Ramirez <ramirezjonathan21@yahoo.com>
2025-04-23 17:37:15 -0500

    Writing_an_Interpreter: Added the ability to print to stdout.

--- writing_an_interpreter/evaluator/builtins.go
+++ writing_an_interpreter/evaluator/builtins.go
@@ -1,7 +1,7 @@
 package evaluator
 
 import (
-	// "unicode/utf8"
+	"fmt"
 	"writing_an_interpreter/object"
 )
 
@@ -95,6 +95,15 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"puts": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 }
